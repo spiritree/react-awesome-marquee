533cda89d4acf7202b639d311f085cde59c33597
spiritree <spiritreess@gmail.com>
2019-01-18 17:22:22 +0800

    refactor: webpack config

--- examples/index.js
+++ examples/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Page from './page'
 
-import './styles/css.css'
-
 const rootDom = document.getElementById('app-root')
 
 ReactDOM.render(<Page />, rootDom)
